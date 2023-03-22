0f188df627a58ba33a584429f971729b35d9d122
gael cardona <cardonagael201@gmail.com>
2023-03-22 08:35:40 -0600

    alianzalimaperu

--- gael.py
+++ gael.py
@@ -10,3 +10,15 @@ print(type(pi))
 print(type(nombre))
 print(type(van_a_ganar_el_ano))
 
+
+'''hola'''
+#hola
+sapa=40
+sapo=40
+
+sapote=sapo+sapa
+print(sapote)
+sapote2=sapote-sapo
+print(sapote2)
+sapote3=sapote2%2
+print(sapote3)
